c7ff41b018d1490bfd962e0a610d18cf710390c2
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-03-31 16:19:51 -0400

    Optimizing security

--- src/main/java/com/careydevelopment/contact/config/WebSecurityConfig.java
+++ src/main/java/com/careydevelopment/contact/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.careydevelopment.contact.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,8 +13,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.careydevelopment.global.config.JwtAuthenticationEntryPoint;
+import com.careydevelopment.global.config.JwtRequestFilter;
 
 @Configuration
 @EnableWebSecurity
